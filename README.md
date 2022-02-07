da474149c24c569e956e452ebf0feefc2ed07ee6
elasticnet12345 <elasticnet1995@gmail.com>
2022-02-08 02:30:12 +0900

    add: basic files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # yokonagashi-video-maker
+
+# Code As Vide (CAV)
+
+## usage
+1. Edit config.json
+2. 
---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+if __name__ == '__main__':
+
+    # parserでオプションを受け取る
+
+    # config.jsonをテストする
+
+    # config.jsonをもとに操作を行う
+
+    #
