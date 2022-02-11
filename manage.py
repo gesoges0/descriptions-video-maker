90a5ca4bdf175869ed4cfe4dc6c9dcf5f8181155
elasticnet12345 <elasticnet1995@gmail.com>
2022-02-11 21:19:19 +0900

    fix: manage

--- manage.py
+++ manage.py
@@ -13,20 +13,25 @@ if __name__ == '__main__':
     # ex: python manage.py -t projectX
 
     # make description images
-    # ex: python manage.py make-image -p projectX
-    # ex: python manage.py make-image --project projectX
-    parser_make_each_image = subparsers.add_parser('make-image', help='make description images')
+    # ex: python manage.py make-images -p projectX
+    # ex: python manage.py make-images --project projectX
+    parser_make_each_image = subparsers.add_parser('make-images', help='make description images')
     parser_make_each_image.add_argument('-p', '--project', type=str, help='project name')
     parser_make_each_image.set_defaults(func=make_description_images)
 
     # concatenate description images to one image
+    # ex: python manage.py concat-images -p projectX
+    # ex: python manage.py concat-images --project projectX
+    # parser_concat_images = subparsers.add_parser('concat-images', help='concatenate description images to one image')
+    # parser_concat_images.add_argument('-p', '--project', type=str, help='project name')
+    # parser_concat_images.set_defaults(func=concatenate_images)
 
     # make video from a concatenated image
-
-    # convert one image to projectX.json
-
-    # layers.jsonからsrc.structures下のクラスを自動的に構築できるようにする
-    # gen / migration
+    # ex: python manage.py make-video -p projectX
+    # ex: python manage.py make-video --project projectX
+    # parser_make_video = subparsers.add_parser('make-video', help='make video from concatenated image')
+    # parser_make_video.add_argument('-p', '--project', type=str, help='project name')
+    # parser_make_video.set_defaults(func=make_video)
 
     args = parser.parse_args()
     args.func(args)
